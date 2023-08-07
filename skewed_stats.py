47a69a07d516c1a4b85e345ce472a3a279d77fb0
martin filliung <martin-filliung@etud.univ-tln.fr>
2023-08-07 09:01:00 +0200

    finished skewed stats

--- skewed_stats.py
+++ skewed_stats.py
@@ -8,8 +8,8 @@ from alive_progress import alive_bar
 warnings.simplefilter(action='ignore', category=Warning)
 
 folder = '/ICRA_EXPORT'
-# w_path = '../../Users/FILLIUNG Martin/OneDrive - Université de Toulon/Thèse/CEPHISMER-11-2022_POST_TRAITEMENT' + folder
-w_path = '../../Users/marti/OneDrive - Université de Toulon/Thèse/CEPHISMER-11-2022_POST_TRAITEMENT' + folder
+w_path = '../../Users/FILLIUNG Martin/OneDrive - Université de Toulon/Thèse/CEPHISMER-11-2022_POST_TRAITEMENT' + folder
+# w_path = '../../Users/marti/OneDrive - Université de Toulon/Thèse/CEPHISMER-11-2022_POST_TRAITEMENT' + folder
 path = os.path.abspath(w_path)
 
 directory = sorted(os.listdir(path))
@@ -27,8 +27,10 @@ for cable in range(1, 9):
     for model in ['vertical', 'inclined']:
         stats[f'{cable}'][model] = {
             'Q1': np.nan,
-            'Q2': np.nan,
-            'Q3': np.nan,
+            'Q25': np.nan,
+            'Q50': np.nan,
+            'Q75': np.nan,
+            'Q99': np.nan,
             'IQR': np.nan,
             'fill': 0,
             'nframes': 0
@@ -45,8 +47,10 @@ for cable in range(1, 9):
                 for model in ['vertical', 'inclined']:
                     stats[f'{cable}'][axis][speed][dis][model] = {
                         'Q1': np.nan,
-                        'Q2': np.nan,
-                        'Q3': np.nan,
+                        'Q25': np.nan,
+                        'Q50': np.nan,
+                        'Q75': np.nan,
+                        'Q99': np.nan,
                         'IQR': np.nan,
                         'fill': 0,
                         'nframes': 0,
@@ -95,17 +99,21 @@ with alive_bar(len(directory), title='Getting data', theme='musical') as bar:
 
 with alive_bar(8, title='Computing statistics', theme='musical') as bar:
     for cable, cable_data in datas.items():
-        stats[cable]['vertical']['Q1'] = cable_data['vertical'].quantile(1 / 4)
-        stats[cable]['vertical']['Q2'] = cable_data['vertical'].quantile(2 / 4)
-        stats[cable]['vertical']['Q3'] = cable_data['vertical'].quantile(3 / 4)
-        stats[cable]['vertical']['IQR'] = stats[cable]['vertical']['Q3'] - stats[cable]['vertical'][
-            'Q1']
+        stats[cable]['vertical']['Q1'] = cable_data['vertical'].quantile(1 / 100)
+        stats[cable]['vertical']['Q25'] = cable_data['vertical'].quantile(1 / 4)
+        stats[cable]['vertical']['Q50'] = cable_data['vertical'].quantile(2 / 4)
+        stats[cable]['vertical']['Q75'] = cable_data['vertical'].quantile(3 / 4)
+        stats[cable]['vertical']['Q99'] = cable_data['vertical'].quantile(99 / 100)
+        stats[cable]['vertical']['IQR'] = stats[cable]['vertical']['Q75'] - stats[cable]['vertical'][
+            'Q25']
 
-        stats[cable]['inclined']['Q1'] = cable_data['inclined'].quantile(1 / 4)
-        stats[cable]['inclined']['Q2'] = cable_data['inclined'].quantile(2 / 4)
-        stats[cable]['inclined']['Q3'] = cable_data['inclined'].quantile(3 / 4)
-        stats[cable]['inclined']['IQR'] = stats[cable]['inclined']['Q3'] - stats[cable]['inclined'][
-            'Q1']
+        stats[cable]['inclined']['Q1'] = cable_data['inclined'].quantile(1 / 100)
+        stats[cable]['inclined']['Q25'] = cable_data['inclined'].quantile(1 / 4)
+        stats[cable]['inclined']['Q50'] = cable_data['inclined'].quantile(2 / 4)
+        stats[cable]['inclined']['Q75'] = cable_data['inclined'].quantile(3 / 4)
+        stats[cable]['inclined']['Q99'] = cable_data['inclined'].quantile(99 / 100)
+        stats[cable]['inclined']['IQR'] = stats[cable]['inclined']['Q75'] - stats[cable]['inclined'][
+            'Q25']
 
         files_data = {'x': cable_data['x'], 'y': cable_data['y']}
         for axis, axis_data in files_data.items():
@@ -113,25 +121,33 @@ with alive_bar(8, title='Computing statistics', theme='musical') as bar:
                 for distance, distance_data in speed_data.items():
                     if type(distance_data['vertical']) != float:
                         stats[cable][axis][speed][distance]['vertical']['Q1'] = \
+                            distance_data['vertical'].quantile(1 / 100)
+                        stats[cable][axis][speed][distance]['vertical']['Q25'] = \
                             distance_data['vertical'].quantile(1 / 4)
-                        stats[cable][axis][speed][distance]['vertical']['Q2'] = \
+                        stats[cable][axis][speed][distance]['vertical']['Q50'] = \
                             distance_data['vertical'].quantile(2 / 4)
-                        stats[cable][axis][speed][distance]['vertical']['Q3'] = \
+                        stats[cable][axis][speed][distance]['vertical']['Q75'] = \
                             distance_data['vertical'].quantile(3 / 4)
+                        stats[cable][axis][speed][distance]['vertical']['Q99'] = \
+                            distance_data['vertical'].quantile(99 / 100)
                         stats[cable][axis][speed][distance]['vertical']['IQR'] = \
-                            stats[cable][axis][speed][distance]['vertical']['Q3'] - \
-                            stats[cable][axis][speed][distance]['vertical']['Q1']
+                            stats[cable][axis][speed][distance]['vertical']['Q75'] - \
+                            stats[cable][axis][speed][distance]['vertical']['Q25']
 
                     if type(distance_data['inclined']) != float:
                         stats[cable][axis][speed][distance]['inclined']['Q1'] = \
+                            distance_data['inclined'].quantile(1 / 100)
+                        stats[cable][axis][speed][distance]['inclined']['Q25'] = \
                             distance_data['inclined'].quantile(1 / 4)
-                        stats[cable][axis][speed][distance]['inclined']['Q2'] = \
+                        stats[cable][axis][speed][distance]['inclined']['Q50'] = \
                             distance_data['inclined'].quantile(2 / 4)
-                        stats[cable][axis][speed][distance]['inclined']['Q3'] = \
+                        stats[cable][axis][speed][distance]['inclined']['Q75'] = \
                             distance_data['inclined'].quantile(3 / 4)
+                        stats[cable][axis][speed][distance]['inclined']['Q99'] = \
+                            distance_data['inclined'].quantile(99 / 100)
                         stats[cable][axis][speed][distance]['inclined']['IQR'] = \
-                            stats[cable][axis][speed][distance]['inclined']['Q3'] - \
-                            stats[cable][axis][speed][distance]['inclined']['Q1']
+                            stats[cable][axis][speed][distance]['inclined']['Q75'] - \
+                            stats[cable][axis][speed][distance]['inclined']['Q25']
 
         bar()
 
@@ -140,14 +156,18 @@ for cable, cable_data in stats.items():
     print(
         f'vertical: '
         f'Q1={cable_data["vertical"]["Q1"]}; '
-        f'Q2={cable_data["vertical"]["Q2"]}; '
-        f'Q3={cable_data["vertical"]["Q3"]}'
+        f'Q25={cable_data["vertical"]["Q25"]}; '
+        f'Q50={cable_data["vertical"]["Q50"]}; '
+        f'Q75={cable_data["vertical"]["Q75"]} '
+        f'Q99={cable_data["vertical"]["Q99"]}'
     )
     print(
         f'inclined: '
         f'Q1={cable_data["inclined"]["Q1"]}; '
-        f'Q2={cable_data["inclined"]["Q2"]}; '
-        f'Q3={cable_data["inclined"]["Q3"]}'
+        f'Q25={cable_data["inclined"]["Q25"]}; '
+        f'Q50={cable_data["inclined"]["Q50"]}; '
+        f'Q75={cable_data["inclined"]["Q75"]} '
+        f'Q99={cable_data["inclined"]["Q99"]}'
     )
     files_data = {'x': cable_data['x'], 'y': cable_data['y']}
     for axis, axis_data in files_data.items():
@@ -159,12 +179,16 @@ for cable, cable_data in stats.items():
                 print(
                     f'\t\t\t\tvertical: '
                     f'Q1={distance_data["vertical"]["Q1"]}; '
-                    f'Q2={distance_data["vertical"]["Q2"]}; '
-                    f'Q3={distance_data["vertical"]["Q3"]}'
+                    f'Q25={distance_data["vertical"]["Q25"]}; '
+                    f'Q50={distance_data["vertical"]["Q50"]}; '
+                    f'Q75={distance_data["vertical"]["Q75"]} '
+                    f'Q99={distance_data["vertical"]["Q99"]}'
                 )
                 print(
                     f'\t\t\t\tinclined: '
                     f'Q1={distance_data["inclined"]["Q1"]}; '
-                    f'Q2={distance_data["inclined"]["Q2"]}; '
-                    f'Q3={distance_data["inclined"]["Q3"]}'
+                    f'Q25={distance_data["inclined"]["Q25"]}; '
+                    f'Q50={distance_data["inclined"]["Q50"]}; '
+                    f'Q75={distance_data["inclined"]["Q75"]} '
+                    f'Q99={distance_data["inclined"]["Q99"]}'
                 )
